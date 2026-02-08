0f3584aa27916265ff79f284803024cee64c4c74
Jimin Lee <17jlee@inertiablogs.com>
2026-02-08 10:48:15 +0000

    Initial Commit

---
+++ VLM_testing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VLM_testing
+//
+//  Created by Jimin Lee on 08/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VLM_testing/VLM_testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  VLM_testingApp.swift
+//  VLM_testing
+//
+//  Created by Jimin Lee on 08/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct VLM_testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
